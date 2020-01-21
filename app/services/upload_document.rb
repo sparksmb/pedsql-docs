125117b2490b767807ae76aef8a992ee6bb471a2
Michael Sparks <sparksmb@gmail.com>
2020-01-20 20:40:52 -0600

    * added UploadDocument class

--- app/services/upload_document.rb
+++ app/services/upload_document.rb
@@ -1,7 +1,15 @@
 require 'net/sftp'
 
 class UploadDocument
-  def self.execute(doc)
-    doc
+  def self.execute(io, file_name)
+    Net::SFTP.start(
+      ENV['PEDSQL_ORG_HOST'],
+      ENV['PEDSQL_ORG_USERNAME'],
+      :password => ENV['PEDSQL_ORG_PASSWORD'],
+      :port => 2222,
+      :non_interactive => true
+    ) do |sftp|
+      sftp.upload!(io, file_name)
+    end
   end
 end
